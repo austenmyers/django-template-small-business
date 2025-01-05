e978918c3d5798555596243258e08231a3565776
Austen Myers <austen.myers@protonmail.com>
2025-01-05 15:54:37 -0700

    created customuser

--- config/settings/_auth.py
+++ config/settings/_auth.py
@@ -14,3 +14,5 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'project_apps.users.CustomUSer'
--- project_apps/users/models.py
+++ project_apps/users/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models as dj
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomerUser(AbstractBaseUser, PermissionsMixin):
+    email = dj.EmailField(
+        _('email_address'),
+        unique=True
+    )
+    is_staff = dj.BooleanField(default=False)
+    is_active = dj.BooleanField(default=True)
+    date_joined = dj.DateTimeField(
+        default=timezone.now
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
